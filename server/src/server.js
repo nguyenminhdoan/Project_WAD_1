3e8905d43a4d4a4e8e462a14d5b263442dbe12da
nguyenminhdoan <nguyenminhdoan0303@gmail.com>
2025-03-17 15:31:42 -0400

    update code

--- server/src/server.js
+++ server/src/server.js
@@ -1,11 +1,27 @@
 require('dotenv').config();
+const cors = require('cors');
 const express = require('express');
 const connectDB = require('./config/db');
 const passport = require('passport');
 const setupRoutes = require('./routes');
 
 
+
 const app = express();
+app.use(cors({
+    origin: function(origin, callback) {
+        // Allow any origin
+        callback(null, true);
+    },
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With']
+}));
+
+// Explicitly handle OPTIONS requests
+app.options('*', cors());
+
+
 connectDB();
 
 app.use(express.json());
@@ -15,5 +31,5 @@ require('./config/passport')(passport);
 setupRoutes(app)
 
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.BACKEND_PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
