b4f1673c45a8ada4fe5fbfb89499a47e368988e8
nguyenminhdoan <nguyenminhdoan0303@gmail.com>
2025-03-19 15:40:16 -0400

    remove unneccessary missing push project

--- Project_WAD_1-main/app/src/pages/HomePage.jsx
+++
@@ -1,5 +0,0 @@
-const HomePage = () => {
-    return <h1>Welcome to Vite React Starter Kit!</h1>;
-};
-
-export default HomePage;
--- Project_WAD_1-main/server/src/routes/index.js
+++
@@ -1,9 +0,0 @@
-const authRoutes = require('./authRoutes');
-const userRoutes = require('./userRoutes');
-// Add other routes here
-
-module.exports = (app) => {
-    app.use('/api/auth', authRoutes);
-    app.use('/api/users', userRoutes);
-    // Register other routes here
-};
