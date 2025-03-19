b376ff42e07b2076a28953effaf9f33eb6fe23d5
Rona Emre Altug <ronaemre7231@gmail.com>
2025-03-19 01:53:18 -0400

    [REA][Registration Page created, GetAllUser endpoint added][navbar edited]

--- Project_WAD_1-main/server/src/routes/authRoutes.js
+++ Project_WAD_1-main/server/src/routes/authRoutes.js
@@ -19,6 +19,7 @@ router.post('/logout', logoutUser); // Token sent in request body
 router.get('/admin', protect, adminOnly, (req, res) => res.json({message: 'Admin access granted'}));
 router.get('/adminTest', protect, testAdmin)
 
+
 // Admin-only route to get all users
 // authRoutes.js
 router.get('/users', getAllUsers); // Kullanıcıları listelemek için
