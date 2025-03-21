cd93eb7919828122e0f3433bfba3ee6388b214ef
nguyenminhdoan <nguyenminhdoan0303@gmail.com>
2025-03-21 14:51:09 -0400

    integrate get and delete product

--- server/src/routes/userRoutes.js
+++ server/src/routes/userRoutes.js
@@ -10,7 +10,7 @@ const {upload} = require("../middleware/uploadMiddleware"); //
 const router = express.Router();
 
 // Protected routes
-router.get('/profile', protect, getUserProfile);
+router.get('/profile/:id', getUserProfile);
 router.put('/profile', protect, upload.single('avatar'), updateUserProfile);
 
 
