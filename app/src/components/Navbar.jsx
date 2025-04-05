20dfffe0d0883b6fd0b10cc3b071a063fa68f0b7
nguyenminhdoan0303@gmail.com <nguyenminhdoan0303@gmail.com>
2025-04-05 00:14:28 -0400

    allow edit photo and user menu shows logo

--- app/src/components/Navbar.jsx
+++ app/src/components/Navbar.jsx
@@ -1,23 +1,11 @@
 import React from 'react';
-import { Link, useNavigate } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import { useSelector } from 'react-redux';
-import { useLogoutMutation } from '../features/auth/authApiSlice';
 import { Button } from '@mui/material';
+import  UserMenu  from './userMenu/UserMenu';
 
 const Navbar = () => {
-    const navigate = useNavigate();
-    const [logout, { isLoading: isLoggingOut }] = useLogoutMutation();
     const isAuthenticated = useSelector((state) => state.auth.isAuthenticated);
-
-    const handleLogout = async () => {
-        try {
-            await logout().unwrap();
-            navigate('/login', { replace: true });
-        } catch (error) {
-            console.error('Logout failed:', error);
-            navigate('/login', { replace: true });
-        }
-    };
 
     return (
         <nav>
@@ -26,39 +14,17 @@ const Navbar = () => {
             {isAuthenticated && (
                 <>
                     <Link to="/dashboard">Dashboard</Link>
-                    <Link to="/profile">Profile</Link>
+                    {/*<Link to="/profile">Profile</Link>*/}
                     <Link to="/product-list">Product List</Link>
                 </>
             )}
-
-            {/*<Link to="/register" style={{ textDecoration: 'none' }}>*/}
-            {/*    <Button style={{ color: 'white' }}>Register</Button>*/}
-            {/*</Link>*/}
 
             {!isAuthenticated ? (
                 <Link to="/login" style={{ textDecoration: 'none' }}>
                     <Button style={{ color: 'white' }}>Login</Button>
                 </Link>
             ) : (
-                <Button
-                    onClick={handleLogout}
-                    variant="contained"
-                    color="secondary"
-                    disabled={isLoggingOut}
-                    sx={{
-                        borderRadius: '8px',
-                        padding: '10px 20px',
-                        fontSize: '16px',
-                        fontWeight: '600',
-                        boxShadow: 3,
-                        '&:hover': {
-                            backgroundColor: '#d32f2f',
-                            boxShadow: 6,
-                        },
-                    }}
-                >
-                    {isLoggingOut ? 'Logging out...' : 'Logout'}
-                </Button>
+                <UserMenu />
             )}
         </nav>
     );
